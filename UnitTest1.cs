5888cd90cdc1edd0227427bed158f5d1bc244088
ric-are-do <ricardoduarte1616@outlook.com>
2025-08-31 12:02:17 +0200

    Basic allure setup

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Allurre_with_Nunit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
